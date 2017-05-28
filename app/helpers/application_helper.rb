05952b3a9375e14bd1b41ec940819e5b13fba769
Pete Hodgson <git@thepete.net>
2017-05-28 11:13:52 -0700

    human timestamp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def human_timestamp(time)
+    content = time_ago_in_words(time) + " ago"
+    content_tag( :time, content, class: 'human-timestamp', time: time.to_s, title: time.to_s )
+  end
 end
