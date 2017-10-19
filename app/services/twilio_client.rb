aa45961790ab307e94fdfa53d336d33b3ceb734d
Pete Hodgson <git@thepete.net>
2017-10-19 12:46:34 -0700

    working twilio client

---
+++ app/services/twilio_client.rb
@@ -0,0 +1,35 @@
+class TwilioClient
+  def initialize
+    @outgoing_number = env_or_bust('TWILIO_OUTGOING_NUMBER')
+
+    @client = Twilio::REST::Client.new(
+      env_or_bust('TWILIO_ACCOUNT_SID'),
+      env_or_bust('TWILIO_AUTH_TOKEN')
+    )
+  end
+
+  def send_sms(number)
+    message = @client.messages.create(
+      body: "Hello from Ruby",
+      from: @outgoing_number,
+      to: number
+    )
+    puts message.sid
+  end
+
+  def call_phone(number)
+    call = @client.calls.create(
+      from: @outgoing_number,
+      to: number,
+      url: 'http://twimlets.com/message?Message%5B0%5D=Thanks%20for%20using%20An%20Button!%20Have%20a%20nice%20day.'
+    )
+    puts call.sid
+  end
+
+  private
+
+  def env_or_bust(key)
+    raise "no #{key} env var provided" unless ENV.has_key?(key)
+    ENV[key]
+  end
+end
